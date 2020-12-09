60743c9583710506ac13f2c774be8e2e1c7cac8d
Walker Lockard <walker@projectdado.com>
2020-12-08 19:49:32 -0600

    uniqueness enforcement for creating user

--- internal/tables/users.go
+++ internal/tables/users.go
@@ -1,11 +1,14 @@
 package tables
 
 import (
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	uuid "github.com/satori/go.uuid"
 	"github.com/wlockiv/walkernews/internal/services"
 	util2 "github.com/wlockiv/walkernews/pkg/util"
+	"strings"
 )
 
 type UserTable struct {
@@ -22,6 +25,7 @@ type User struct {
 // TODO: Should this be taking UN & Password directly?
 func (ut *UserTable) Put(username, password string) (*User, error) {
 	userId := uuid.NewV4().String()
+
 	hashedPassword, err := util2.HashPassword(password)
 	if err != nil {
 		return nil, err
@@ -33,21 +37,50 @@ func (ut *UserTable) Put(username, password string) (*User, error) {
 		Password: hashedPassword,
 	}
 
-	av, err := dynamodbattribute.Marshal(newUser)
-	if err != nil {
+	var transactWriteItems []*dynamodb.TransactWriteItem
+
+	// For the actual user record
+	if newUserAV, err := dynamodbattribute.Marshal(newUser); err != nil {
 		return nil, err
+	} else {
+		transactWriteItem := &dynamodb.TransactWriteItem{
+			Put: &dynamodb.Put{
+				TableName:           &ut.tableName,
+				ConditionExpression: aws.String("attribute_not_exists(id)"),
+				Item:                newUserAV.M,
+			},
+		}
+
+		transactWriteItems = append(transactWriteItems, transactWriteItem)
 	}
 
-	dynamoInput := &dynamodb.PutItemInput{
-		Item:      av.M,
-		TableName: &ut.tableName,
-	}
-
-	if _, err := ut.dynamodb.PutItem(dynamoInput); err != nil {
+	// For the username hash (enforcing uniqueness)
+	if usernameHashAV, err := dynamodbattribute.Marshal(map[string]string{"id": "username#" + username}); err != nil {
 		return nil, err
+	} else {
+		transactWriteItem := &dynamodb.TransactWriteItem{
+			Put: &dynamodb.Put{
+				TableName:           &ut.tableName,
+				ConditionExpression: aws.String("attribute_not_exists(id)"),
+				Item:                usernameHashAV.M,
+			},
+		}
+
+		transactWriteItems = append(transactWriteItems, transactWriteItem)
 	}
 
-	return &User{ID: userId, Username: username}, nil
+	transactWriteItemsInput := &dynamodb.TransactWriteItemsInput{TransactItems: transactWriteItems}
+
+	if _, err := ut.dynamodb.TransactWriteItems(transactWriteItemsInput); err != nil {
+		if strings.Contains(err.Error(), "ConditionalCheckFail") {
+			return nil, errors.New(`the requested username, '` + username + `', has already been claimed`)
+		}
+
+		return nil, err
+	} else {
+		return &User{ID: userId, Username: username}, nil
+	}
+
 }
 
 func GetUserTable() *UserTable {
